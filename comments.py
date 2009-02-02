b1c5c87fb56ce6d4ba2facdc33f64e367d8e2252
chromakode <chromakode@gmail.com>
2009-02-01 23:24:16 -0800

    Fix indentation (accidentally had JS-style indentation on)

--- comments.py
+++ comments.py
@@ -2,5 +2,5 @@ import urllib
 import simplejson
 
 def fetch_latest_comments():
-  comment_listing = simplejson.load(urllib.urlopen("http://www.reddit.com/comments.json"))
-  return [ entry["data"] for entry in comment_listing["data"]["children"] ]
+    comment_listing = simplejson.load(urllib.urlopen("http://www.reddit.com/comments.json"))
+    return [ entry["data"] for entry in comment_listing["data"]["children"] ]
