fc0819b524c43543000f4e5fc9dcb7a5bab58c2b
chromakode <chromakode@gmail.com>
2009-02-06 01:04:14 -0800

    Classy token classes.

--- markov.py
+++ markov.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
+import sys
 import re
 import random
 import bisect
-import sys
+import itertools
 from zlib import crc32
 
 def weighted_choice(weight_dict):
@@ -43,24 +44,112 @@ def merge_countlists(countlists):
 def has_method(instance, name):
   return hasattr(instance, name) and callable(getattr(instance, name))
 
-START_TOKEN  = '\x02'
-END_TOKEN    = '\x03'
-SPECIAL_TOKENS = [START_TOKEN, END_TOKEN]
+class TokenType:
+    def is_match(cls, text): raise NotImplementedError
+    def find_all(cls, text): raise NotImplementedError
+    
+class RegexTokenType(TokenType):
+    def __init__(self, regex):
+        self.regex = re.compile(regex)
+    
+    def is_match(self, text):
+        return self.regex.match(text) is not None
+    
+    def find_all(self, text):
+        return [(m.group(0), m.start()) for m in self.regex.finditer(text)]
 
-class Token(str):
-    types = {'special':     lambda x: x in SPECIAL_TOKENS,
-             'punctuation': re.compile(r'([^\w\s%s]+)' % ''.join(SPECIAL_TOKENS)),
-             'word':        re.compile(r'(\w+)')}
+class SpecialTokenType(TokenType):
+    def __init__(self, **characters):
+        self.characters = characters
+        
+    def __getitem__(self, key):
+        return self.characters[key]
+    
+    def is_match(self, text):
+        return text in self.characters.values()
+    
+    def find_all(self, text):
+        return []
+            
+class Token:
+    SpecialToken     = SpecialTokenType(start='\x02', end='\x03')
+    WordToken        = RegexTokenType(r'(\w+)')
+    PunctuationToken = RegexTokenType(r'([^\w\s%s]+)' % ''.join(SpecialToken.characters.values()))
+    UnknownToken     = TokenType()
 
-    @property
-    def token_name(self):
-        for (name, test) in self.types.iteritems():
-            re_matches = lambda t: has_method(t, 'match') and t.match(self)
-            call_matches = lambda t: callable(t) and t(self)
-            if re_matches(test) or call_matches(test):
-                return name
+    types = [SpecialToken, WordToken, PunctuationToken]
 
-# chain =:= dict(key = dict(follower = count))
+    _joins = {(PunctuationToken,WordToken,'\'')   : '',
+              (PunctuationToken,WordToken)        : ' ',
+              (PunctuationToken,PunctuationToken) : '',
+              (WordToken,WordToken)               : ' ',
+              None                                : ''}
+    
+    class TokenClass(str):
+        def __new__(self, text, token_type):
+            return str.__new__(self, text)
+        
+        def __init__(self, value, token_type):
+            self.token_type = token_type
+    
+    @classmethod
+    def join(cls, tokens):
+        output = list()
+        
+        tokens = [None] + list(tokens) + [None]
+        for tokens in itertools.izip(tokens, tokens[1:]):
+            types = []
+            texts = []
+            for token in tokens:
+                if token:
+                    types.append(token.token_type)
+                    texts.append(str(token))
+                else:
+                    types.append(None)
+                    texts.append(None)
+
+            join_keys = (#(types[0],types[1],texts[0],texts[1]),
+                         (types[0],types[1],texts[0]),
+                         #(types[0],types[1],None,texts[1]),
+                         (types[0],types[1]),
+                         #(types[0]),
+                         #(None,types[1])
+                         )
+            
+            sep = cls._joins[None]
+            for join_key in join_keys:
+                if join_key in cls._joins:
+                    sep = cls._joins[join_key]
+                    break
+            
+            output.append(sep)
+            if tokens[1]: output.append(tokens[1])
+            
+        return ''.join(output)
+            
+    @classmethod
+    def tokenize(cls, text):
+        '''Return tokens contained in text in order'''
+        matches = []
+        for token_type in cls.types:
+            matches += token_type.find_all(text)
+        
+        tokens = [cls.new(m[0]) for m in sorted(matches, key=lambda m: m[1])]
+        tokens.insert(0, cls.SpecialToken["start"])
+        tokens.append(cls.SpecialToken["end"])
+        
+        return tokens
+    
+    @classmethod
+    def new(cls, text):
+        this_token_type = cls.UnknownToken
+        for token_type in cls.types:
+            if token_type.is_match(text):
+                this_token_type = token_type
+                break
+        
+        return cls.TokenClass(text, this_token_type)
+
 class Chain(dict):
     def __init__(self, basis=None, N=1, mhash=crc32):
         if isinstance(basis, dict):
@@ -70,18 +159,7 @@ class Chain(dict):
         self.mhash = mhash
        
     def train(self, text):
-        text = text.lower()
-        
-        # Find all tokens in text
-        matches = []
-        for (name, test) in Token.types.iteritems():
-            if has_method(test, 'finditer'):
-                matches += test.finditer(text)
-        
-        start_pos = lambda m: m.start()
-        tokens = [m.group(0) for m in sorted(matches, key=start_pos)]
-        tokens.insert(0, START_TOKEN)
-        tokens.append(END_TOKEN)
+        tokens = Token.tokenize(text.lower())
 
         for x in range(len(tokens)):
             for y in range(1, self.N+1):
@@ -94,11 +172,11 @@ class Chain(dict):
                     else:
                         self[before_hash] = {me: 1}
 
-    def generate(self, words=[START_TOKEN], maxlength=None):
+    def generate(self, words=[Token.SpecialToken["start"]], maxlength=None):
         words = list(words)
         
         picked = None
-        while picked != END_TOKEN and (len(words) < maxlength or maxlength is None):
+        while picked != Token.SpecialToken["end"] and (len(words) < maxlength or maxlength is None):
             # Truncate previous word list to the length of the max association distance (N)
             if len(words) > self.N:
                 words = words[-self.N:]
@@ -120,6 +198,9 @@ class Chain(dict):
             else:
                 raise ValueError('No candidate words available.')
             
-            yield Token(picked)
+            yield Token.new(picked)
 
             words.append(picked)
+            
+    def generate_text(self, words=[Token.SpecialToken["start"]], maxlength=None):
+        return Token.join(self.generate(words, maxlength))
